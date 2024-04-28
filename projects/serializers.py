251e657c97f1bdc308dbdff9defba36a8ae18336
Ferney Avila <161901153+BDA824@users.noreply.github.com>
2024-04-28 23:12:47 +0000

    Initial commit

---
+++ projects/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Project
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        # Estos son los campos de mi tabla Project que voy a mostrar en la respuesta al cliente
+        fields = ('id', 'tittle', 'description', 'technology', 'create_at')
+        # Este campo solo se podra visualizar y no editar
+        read_only_fields = ('create_at')
